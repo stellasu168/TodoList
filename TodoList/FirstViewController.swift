9d030ef5deea198cdefe932de1a7c335cf94efad
stellasu168 <millionstars@gmail.com>
2016-02-13 01:42:55 -0800

    Initial Commit

---
+++ TodoList/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TodoList
+//
+//  Created by Stella Su on 2/13/16.
+//  Copyright © 2016 Million Stars, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
